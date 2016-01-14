04add7a2a92a16fd0bd19694d1cdc8dbe815a639
Francisco José A. C. Souza <francisceioseph@gmail.com>
2016-01-14 19:26:27 -0300

    - Feature: Create viewa for awards and it implementation

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114140456) do
+ActiveRecord::Schema.define(version: 20160114220015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "awards", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.string   "requisite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "classrooms", force: :cascade do |t|
     t.string   "access_code"
