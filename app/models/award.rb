51ccac1ae0ac057851e5283133c17172cf2adbdf
Francisco José A. C. Souza <francisceioseph@gmail.com>
2016-01-14 19:07:08 -0300

    - Feature: Create missed relationships

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,4 +1,7 @@
 class Award < ActiveRecord::Base
+
+  belongs_to :classroom
+
   validates_presence_of :title
   validates_presence_of :text
   validates_presence_of :requisite
--- app/models/exercise_list.rb
+++ app/models/exercise_list.rb
@@ -2,5 +2,7 @@ class ExerciseList < ActiveRecord::Base
   has_many :questions
   has_many :exercise_answers
 
+  belongs_to :classroom
+
   validates_presence_of :title
 end
