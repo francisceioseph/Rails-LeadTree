e8a7c98f5cfec28f306fdd3c1eac7d64a3446756
Francisco José A. C. Souza <francisceioseph@gmail.com>
2016-01-19 14:56:20 -0300

    Add Teacher and Student Models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up){ |u| u.permit( :type, :email, :password, :password_confirmation ) }
+  end
+
 end
