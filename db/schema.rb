c9427665d7a930c239a3af22d160a9fb2158578d
Francisco José A. C. Souza <francisceioseph@gmail.com>
2016-01-19 15:27:42 -0300

    Add User as comment author

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119182319) do
+ActiveRecord::Schema.define(version: 20160119182603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 20160119182319) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "post_id"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "enrollments", force: :cascade do |t|
     t.datetime "created_at",   null: false
